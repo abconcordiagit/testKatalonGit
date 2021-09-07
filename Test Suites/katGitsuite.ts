fc45539a11515ee7447c7deaf4540c3023f0130a
amahmud <amahmud@TMXPC237.thinkmax.com>
2021-09-07 09:43:43 -0400

    Commit katGitsuite 1

---
+++ Test Suites/katGitsuite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>katGitsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>32aecc57-fd61-4863-80c8-026d754f0a4b</testSuiteGuid>
+   <testCaseLink>
+      <guid>4ac3b58f-ca7a-45dc-be58-050972f11109</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ae9e240d-a9b7-43ee-aad6-00dbef5a64a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
